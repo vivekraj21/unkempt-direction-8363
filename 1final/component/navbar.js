60b018bf5800d324c39ccd64e1f630bd5bd7bde6
Vivek Raj <vivekrajrollno21@gmail.com>
2022-07-23 21:40:04 +0530

    final_file_by_fw19_1215

--- 1final/component/navbar.js
+++ 1final/component/navbar.js
@@ -1,39 +1,40 @@
 let navbar= ()=>{
     return ` <div id="navbar_1st">
     <a href="https://imgbb.com/"><img src="https://www.zee5.com/images/ZEE5_logo.svg?ver=2.52.17" alt="Zee5" border="0"></a>
-    <h3><a id="link1" href="#">Home</a></h3>
-    <h3><a class="link" href="#">Tv Shows</a></h3>
-    <h3><a class="link" href="#">Movies</a></h3>
-    <h3><a class="link" href="#">Web Series</a></h3>
-    <h3><a class="link" href="#">News</a></h3>
+    <h3><a id="link1" href="navbar.html">Home</a></h3>
+    <h3><a class="link" href="tvshow_page.html">Tv Shows</a></h3>
+    <h3><a class="link" href="navbar.html">Movies</a></h3>
+    <h3><a class="link" href="webseries_page.html">Web Series</a></h3>
+    <h3><a class="link" href="news.html">News</a></h3>
     <i id="i1" class="fa fa-list-ul"></i>
    
     <i id="i2" class="fa fa-search" aria-hidden="true"></i>
 </div>
 
 <div id="navbar_2nd">
-    <input type="text" id="search">
+   
+    <a id="search" href="SearchQ.html" class="query"></a>
     <h1 id="lang">A</h1>
     <a href="https://imgbb.com/"><img src="https://i.ibb.co/7jtpcHz/aa1.png" alt="aa1" border="0"></a>
-    <button id="loginbtn">LOGIN</button>
+    <button id="loginbtn"><a id="ram" href="signup_page.html">LOGIN</a> </button>
     
-    <button id="plan"><i id="i3" class="fas fa-chess-queen"></i>BUY PLAN</button>
+    <button id="plan"><i id="i3" class="fas fa-chess-queen"></i><a href="plan.html" id="om">BUY PLAN</a></button>
     
     <div id="right">
     <i id="i4" class="fas fa-qrcode"></i>
     <div id="dropdown">
                 <div>
                     
-                    <a id="anc1" href="">Kids</a>
+                    <a id="anc1" href="kid_page.html">Kids</a>
                    
                 </div>
               <div>
                
-                <a href="">Videos</a>
+                <a href="navbar.html">Videos</a>
               </div>
                <div>
                 
-                <a href="">Play</a>
+                <a href="webseries_page.html">Play</a>
                </div>
                 <div>
                     
@@ -45,7 +46,7 @@ let navbar= ()=>{
                </div>
                <div>
                 
-                <a href="">ZEE Plex</a>
+                <a href="navbar.html">ZEE Plex</a>
                </div>
                 <div>
                     
@@ -59,7 +60,7 @@ let navbar= ()=>{
                 </div>
                <div>
                 
-                <a  href="">Chennels</a>
+                <a  href="">Channels</a>
                </div>
              </div>
            </div>  
--- 1final/navbarimport.js
+++ 1final/navbarimport.js
@@ -5,25 +5,22 @@ document.getElementById("navbar").innerHTML=navbar();
 
 let plan=document.getElementById("plan");
 plan.addEventListener("click",function(){
-    window.location.href="./plan.html"
-    
+
+    console.log("my");
 })
 
-// document.querySelector("#plan").addEventListener("click",function(){
-//     document.querySelector("#popup").style.display="flex"
-//         })
+document.querySelector("#plan").addEventListener("click",function(){
+    document.querySelector("#popup").style.display="flex"
+        })
         
-//  document.querySelector("#crossbtn").addEventListener("click",function(){
-//  document.querySelector("#popup").style.display="none"
-//  })       
+ document.querySelector("#crossbtn").addEventListener("click",function(){
+ document.querySelector("#popup").style.display="none"
+ })       
 
  let pay=document.getElementById("continuewithplan").addEventListener("click",function(){
   window.location.href="address.html"
 
  })
-
-
-
 
 
 
